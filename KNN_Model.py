1e2272a75f6ec39ca23d8aaacc7e36d1b92282b1
Bartłomiej Pietrzyk <barpietrzyk@gmail.com>
2024-01-11 15:25:28 +0100

    changes to code - 3

---
+++ KNN_Model.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+
+#preprocessing data
+data = pd.read_excel('waluty.xlsx', skiprows=2)
+data = data[['Data', 'Kurs', 'Waluta']]
+data['Data'] = pd.to_datetime(data['Data'])
+cut_of_date = pd.Timestamp('2023-01-01')
+training_data_set = data[data['Data'] < cut_of_date]
+test_data_set = data[data['Data'] > cut_of_date]
+
+def create_windows(data, windows_size=3):
+    windows = [data[i:i+windows_size] for i in range(len(data) - windows_size + 1)]
+    return windows
+
+x_train = create_windows(training_data_set['Kurs'].values)
+x_test = create_windows(test_data_set['Kurs'].values)
+
+#1. Initiation
+knn = NearestNeighbors(n_neighbors = 3)
+#2. Fitting
+knn.fit(x_train)
+
+########################################################################################
+
+from sklearn.neighbors import NearestNeighbors
+
+class Model:
+
+    """
+    The class represents the k-nearest neighbors algorithm that takes a threshold
+     as an initiator and methods that fit, train and return an anomaly score based on the given dataset.
+    """
+
+    def __init__(self, k, threshold):
+      self.knn = NearestNeighbors(n_neighbors = k)
+      self.threshold = threshold
+      self.lista = []
+
+    def fit(self, training_data_set):
+      x_train = create_windows(training_data_set['Kurs'].values)
+      self.knn.fit(x_train)
+
+    def predict(self, test_data_set):
+      x_test = create_windows(test_data_set['Kurs'].values)
+      distance, indices = self.knn.kneighbors(x_test)
+      return distance
+
+    def anomaly_score(self, test_data_set):
+    #został przekroczony threshold to 1
+      for item in self.predict(test_data_set):
+        if item[-1] > self.threshold:
+          self.lista.append(1)
+        else:
+          self.lista.append(0)
+      return self.lista
--- currency_tracker.py
+++ currency_tracker.py
@@ -1,74 +1,108 @@
 import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from datetime import datetime
+
+from_email = "barpietrzyk@gmail.com"
+to_email = "barpietrzyk@gmail.com"
+password = "judw ijwp dnvx bohs"
+
+def send_email(subject, message, to_email, from_email, password):
+  msg = MIMEMultipart()
+  msg['Subject'] = subject
+  msg['From'] = from_email
+  msg['To'] = to_email
+  msg.attach(MIMEText(message, 'plain'))
+
+  with smtplib.SMTP('smtp.gmail.com', 587) as server:
+      server.starttls()
+      server.login(from_email, password)
+      server.send_message(msg)
 
 class CurrencyTracker:
+
+  """
+The class represents a CSV file and list of thresholds as attributes and methods that load the data,
+ compare rates with the given thresholds, set and check alerts, and send notification to the destination user.
+  """
   def __init__(self, CSV_Path):
     self.CSV_Path = CSV_Path
     self.df = self.load_currency_data()
     self.thr_dict = []
+    self.currency_value_column = 'Kurs'
+    self.data_column = 'Data'
+    self.currency_code = 'Waluta'
 
   def load_currency_data(self):
-    """it reads data from the file created and filled in 
-       the previous step (currency exchange data crawler)
-       read_excel instead of read_csv
-    Returns:
-        it returns dataframe
-    """
+    """It reads data from the file created and filled in
+   	    the previous step (scrapping data)
+   	    read_excel instead of read_csv.
+	  Returns:
+    	It returns dataframe.
+	  """
     df = pd.read_excel(self.CSV_Path)
     return df
 
   def comparison(self, currency_code, treshold_low, treshold_high):
-    """it checks if there are any items above/below given threshold       
-    Args:
-        currency_code (string): selected currency code
-        treshold_low (float): selected threshold
-        treshold_high (float): selected threshold
+      """It checks if there are any items above/below given threshold.
+	      Args:
+    	  currency_code (string): selected currency code
+    	  treshold_low (float): selected threshold
+    	  treshold_high (float): selected threshold
 
-    Returns:
-        if exists, it returns the list of items below and/or above threshold.
-    """
-    is_greater_than_treshold = self.df[currency_code].gt(treshold_high).any()
-    
-    if is_greater_than_treshold:
-      above_treshold = self.df[self.df[currency_code] > treshold_high]
-      above_treshold_list = above_treshold['Data'].list()
-    else:
-      above_treshold_list = []
-      
-    is_lower_than_treshold = self.df[currency_code].lt(treshold_low).any()
+	      Returns: Robi detekcje anomalii w oparciu o górny i dolny threshold. Zwraca liste dat kiedy waluta przekroczyla threshold dolny i górny
+    	  if exists, it returns the list of items below and/or above threshold.
+	    """
+      self.df = self.df[self.df[self.currency_code] == currency_code]
+      is_greater_than_treshold = self.df[self.currency_value_column].gt(treshold_high).any()
+      if is_greater_than_treshold:
+        above_treshold = self.df[self.df[self.currency_value_column] > treshold_high]
+        above_treshold_list = above_treshold[self.data_column].tolist()
+      else:
+        above_treshold_list = []
 
-    if is_lower_than_treshold:
-      below_treshold = self.df[self.df[currency_code] < treshold_low]
-      below_treshold_list = below_treshold['Data'].list()
-    else:
-      below_treshold_list = []
-    return below_treshold_list, above_treshold_list
+      is_lower_than_treshold = self.df[self.currency_value_column].lt(treshold_low).any()
+
+      if is_lower_than_treshold:
+        below_treshold = self.df[self.df[self.currency_value_column] < treshold_low]
+        below_treshold_list = below_treshold[self.data_column].tolist()
+      else:
+        below_treshold_list = []
+      return below_treshold_list, above_treshold_list
 
   def set_alert(self, currency_code, treshold_low, treshold_high):
-    """The method enables to set destined thresholds and currency 
-      code in the form of dictionary.
-
-    Args:
-        currency_code (string): selected currency code
-        treshold_low (float): selected threshold
-        treshold_high (float): selected threshold
-    """
+    """The method enables to set destined thresholds and currency
+  	    code in the form of a dictionary.
+	  Args:
+    	  currency_code (string): selected currency code
+    	  treshold_low (float): selected threshold
+    	  treshold_high (float): selected threshold
+	  """
     self.thr_dict.append({
         'currency': currency_code,
         'low': treshold_low,
-        'high': treshold_high
-    })
+        'high': treshold_high})
 
   def check_alerts(self):
-    """It checks whether determined thresholds are 
+    """It checks given threshold values with the data and if any appears to met them
+        sends notification with the listed values.
     """
     for item in self.thr_dict:
-      above_treshold_list, below_treshold_list = self.comparison(item['currency'], item['low'], item['high'])
+      above_treshold_list, below_treshold_list = self.comparison(item['currency'], item['low'], item['high'])#popraw
       if len(above_treshold_list) > 0 | len(below_treshold_list):
-        self.notification(item['currency'], below_treshold_list, above_treshold_list)
+        below_treshold_string = ", ".join(below_treshold_list)
+        above_treshold_string = ", ".join(above_treshold_list)
+        self.send_notification(item['currency'], below_treshold_string, above_treshold_string)#popraw
 
+  def send_notification(self, currency_code, below_treshold_string, above_treshold_string):
+    message = f"""
+          Dzień: {datetime.now().date()}
+          Kurs: {currency_code}
+          Dla podanej waluty i podanych tresholdow lista z ostatniego miesiaca.
+          Poniżej threshold: {below_threshold_string}
+          Powyżej thresholdu: {above_threshold_string}
+          """
 
-  def send_notification(self, currency_code, below_treshold_list, above_treshold_list):
-    """It sends notification to the destined email with the list of elements 
-    which exceeds given threshold.
-     """
-    pass
+    subject = f"Alert anomalii w kursie {currency_code}"
+    send_email(subject, message, to_email, from_email, password)
